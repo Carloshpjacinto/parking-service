9178479502f1419a654fc7b4e73bda2d1321de62
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-08-12 03:36:18 -0300

    feature/create-api

---
+++ vehicle/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VehicleTypeViewSet, VehicleViewSet
+
+router = DefaultRouter()
+router.register('vehicle/type', VehicleTypeViewSet)
+router.register('vehicle', VehicleViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
